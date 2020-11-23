6fb29e1199a063dcec2dd140d7d26e9c41ae8220
AndresGardea <70489589+AndresGardea@users.noreply.github.com>
2020-11-23 01:24:43 -0700

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,11 @@
+<?php
+
+$destino = "unfotografomascuu@gmail.com";
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$mensaje = $_POST["mensaje"];
+$contenido = "Nombre: " . $nombre . "\nCorreo:" . $correo . "\nMensaje:" . $mensaje;
+mail($destino,"Correo enviado con el Footer de la Pagina",$contenido);
+header("Location:index.html");
+
+?>
